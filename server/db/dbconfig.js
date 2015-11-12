c548f206e0948e37064c8184c8fa7cb1725166cc
Hack Reactor Students <accounts@hackreactor.com>
2015-11-11 16:58:57 -0800

    Model definitions on database done, added mysql as dependency

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,7 +1,9 @@
+var mysql = require('mysql');
 var Sequelize = require('sequelize');
 var secret = require('../lib/secrets').sql
 
 var sequelize = new Sequelize('sniphub', 'root', secret);
+
 
 var User = sequelize.define('User', {
   username: {
@@ -12,4 +14,20 @@ var User = sequelize.define('User', {
   imgUrl: Sequelize.STRING
 });
 
-var Snippets = sequelize.define('Snippets')
+var Snippets = sequelize.define('Snippets', {
+  text : Sequelize.STRING,
+  forkedCount : Sequelize.INTEGER
+});
+
+var Tags = sequelize.define('Tags', {
+  tagname: Sequelize.STRING
+});
+
+User.hasMany(Snippets);
+Snippets.belongsTo(User);
+Snippets.hasMany(Tags);
+Tags.hasMany(Snippets);
+
+User.sync();
+Snippets.sync();
+Tags.sync();
