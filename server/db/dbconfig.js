9e44ef829189a5d90709315f240a46f6047528fa
tlevin <toddlevin802@gmail.com>
2015-11-11 18:37:44 -0800

    Documentation of database complete

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,11 +1,22 @@
+// # Database configuration
+
+//We use sequelize ORM to connect to MySql database.
+//Credentials for the database are stored in /lib/secrets.js
+//To use with your own database recreate the file and export an
+//object with the key 'sql' and your password as the value
+
+
+// Declare dependencies
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
 var secret = require('../lib/secrets').sql;
 
+// Connection to MySql database using database named sniphub
+
 var sequelize = new Sequelize('sniphub', 'root', secret);
 
 
-var User = sequelize.define('User', {
+var User = sequelize.define('users', {
   username: {
     type: Sequelize.STRING,
     unique: true
@@ -19,20 +30,24 @@ var Snippets = sequelize.define('snippets', {
   forkedCount : Sequelize.INTEGER
 });
 
+
 var Tags = sequelize.define('tags', {
   tagname: Sequelize.STRING
 });
 
-var SnippetTags = sequelize.define('snippet_tags', {
+// Declare join table to be used in Many to Many relationship
+var SnippetTag = sequelize.define('snippet_tag', {
 
-})
-
+});
+// Creates one to many relationship between User and Snippets table
 User.hasMany(Snippets);
 Snippets.belongsTo(User);
-Snippets.belongsToMany(Tags, { through: 'snippet_tags' });
-Tags.belongsToMany(Snippets, { through: 'snippet_tags' });
+
+// Creates many to many relationship between Snippets and Tags
+Snippets.belongsToMany(Tags, { through: 'snippet_tag'});
+Tags.belongsToMany(Snippets, { through: 'snippet_tag'});
 
 User.sync();
 Snippets.sync();
 Tags.sync();
-SnippetTags.sync();
+SnippetTag.sync();
