b3dc7713f957389850cefd57ca3a3c747046c858
Lauren <laurenmendoza@gmail.com>
2015-11-12 11:49:27 -0800

    fixed angular errors, installed polymer

--- client/js/services.js
+++ client/js/services.js
@@ -72,29 +72,29 @@ angular.module('sniphub.services', [])
   // after you signin/signup open devtools, click resources,
   // then localStorage and you'll see your token from the server
   var signin = function (user) {
-    return $http({
-      method: 'POST',
-      url: '/api/users/signin',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
+    // return $http({
+    //   method: 'POST',
+    //   url: '/api/users/signin',
+    //   data: user
+    // })
+    // .then(function (resp) {
+    //   return resp.data.token;
+    // });
   };
 
   var signup = function (user) {
-    return $http({
-      method: 'POST',
-      url: '/api/users/signup',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
+    // return $http({
+    //   method: 'POST',
+    //   url: '/api/users/signup',
+    //   data: user
+    // })
+    // .then(function (resp) {
+    //   return resp.data.token;
+    // });
   };
 
   var isAuth = function () {
-    return !!$window.localStorage.getItem('com.sniphub');
+    // return !!$window.localStorage.getItem('com.sniphub');
   };
 
   var signout = function () {
