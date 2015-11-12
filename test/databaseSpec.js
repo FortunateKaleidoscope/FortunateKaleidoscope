ef7bd648e9b24943146b1f86f088b88b8745f50c
tlevin <toddlevin802@gmail.com>
2015-11-11 20:13:15 -0800

    Tests

--- test/databaseSpec.js
+++ test/databaseSpec.js
@@ -68,4 +68,59 @@ describe("Snippets Table", function () {
     });
   });
 
+describe("Tags Table", function () {
+  beforeEach(function ( done ) {
+    var sequelize = new Sequelize('sniphub', 'root', 'test');
+    db.Tags.destroy({ where: { tagname: "javascript"}
+    });
+    sequelize.sync({force: true})
+    .then( function () {
+      done();
+    });
+});
+
+    it("Should allow tags to be created", function (done) {
+      db.Tags.create({tagname: "javascript" }).then(function () {
+        db.Tags.findOne({ where: { tagname : "javascript" } }).then(function (tag) {
+          expect(tag.tagname).to.equal("javascript");
+          done();
+        });
+      });
+    });
+
+    it("Should allow tags to be deleted", function (done) {
+      db.Tags.create({tagname: "javascript", forkedCount: 1})
+      .then( function () { db.Tags.destroy({ where: { tagname: "javascript" } }) })
+      .then( function () { db.Tags.findOne({ where: { tagname: "javascript" } }) })
+      .then( function (tag) {
+          expect(tag).to.equal(undefined);
+          done();
+      });
+    });
+  });
+
+describe("Integration of tables", function () {
+  beforeEach(function ( done ) {
+    var sequelize = new Sequelize('sniphub', 'root', 'test');
+    db.Tags.destroy({ where: { tagname: "javascript"} });
+    db.User.destroy({ where : {username: "fred" } });
+    db.Snippets.destroy({ where: {text: "awesome text snippet"}});
+    sequelize.sync({force: true})
+    .then( function () {
+      done();
+    });
+  });
+
+  it("Should correctly link associations across tables", function (done) {
+      db.Snippets.create({text: "awesome text snippet", User: { username: "fred"}, Tags: { tagname: "javascript" }}, { include: [User, Tags]})
+      .then( function () { 
+        db.Snippets.findOne({ where: { text: "awesome text snippet" } }); 
+      })
+      .then( function ( snippet ) {
+        expect(snippet.username).to.equal("fred");
+        expect(snippet.text).to.equal("awesome text snippet")
+      });
+    });
+
+
 });
