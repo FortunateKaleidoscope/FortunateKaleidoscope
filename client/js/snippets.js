fb855aced182d4eb1ccde0d6923ada8fe4c967b4
tlevin <toddlevin802@gmail.com>
2015-11-13 15:49:51 -0800

    working on displaying unescaped data

--- client/js/snippets.js
+++ client/js/snippets.js
@@ -1,14 +1,17 @@
 // FIX ALL THIS
-angular.module('sniphub.snippets', [])
+angular.module('sniphub.snippets', ['ngSanitize'])
 
-.controller('SnippetsController', function (Auth, $scope, $state, $stateParams, $location, SniphubServices) {
+.controller('SnippetsController', function (Auth, $scope, $state, $stateParams, $location, SniphubServices, Utility, $sce) {
   $scope.snippets = [];
   $scope.fetchTopTen = function () {
     //call factory function
     SniphubServices.fetchTopTen()
       .then(function ( snippets ) {
         $scope.snippets = snippets.data;
-        $scope.inUserView = false;
+        $scope.snippets.forEach(function (item) {
+          // $scope.unescape(item.text);
+          console.log(item.text)
+        });
       });
 
   };
@@ -18,8 +21,14 @@ angular.module('sniphub.snippets', [])
     SniphubServices.fetchByUser( user )
       .then(function ( snippets ) {
         $scope.snippets = snippets.data;
-        $scope.inUserView = true;
+        // $scope.snippets.forEach(function (item) {
+        //   $scope.unescape(item.text);
+        // });
       });
+  };
+
+  $scope.unescape = function ( string ) {
+    return Utility.unescapeQuotes( string );
   };
   // EXTRA CREDIT
   // $scope.searchByTerm = function ( term ) {
