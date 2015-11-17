9817f1398bc56c4ac811d67010e06e7a350b0feb
Lauren <laurenmendoza@gmail.com>
2015-11-16 16:16:09 -0800

    removed HEAD merge conflict

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,4 @@
 angular.module('sniphub', ['ngRoute','ui.router','sniphub.services','sniphub.snippets','sniphub.addSnippet', 'sniphub.auth', 'sniphub.snippetsUser','sniphub.editSnippet','sniphub.angular-ellipsis'])
-
-<<<<<<< HEAD
-=======
-angular.module('sniphub', ['ngRoute','ui.router','sniphub.services','sniphub.snippets','sniphub.addSnippet', 'sniphub.auth', 'sniphub.snippetsUser','sniphub.angular-ellipsis'])
->>>>>>> 822978f5a8bf38ed73277643d69fffbe94fa9653
 .config(function($stateProvider, $urlRouterProvider, $httpProvider){
   $urlRouterProvider.otherwise('snippets');
   $stateProvider
