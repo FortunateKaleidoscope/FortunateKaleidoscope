83b2b730572b4ef50710eb0067afa021c5d1a8cf
tlevin <toddlevin802@gmail.com>
2015-11-16 16:31:19 -0800

    Commenting clientside

--- client/js/auth.js
+++ client/js/auth.js
@@ -3,7 +3,9 @@ angular.module('sniphub.auth', ['sniphub.services'])
 .controller('AuthController', function ($scope, $window, $location, Auth) { //Auth
   $scope.isAuth = false;
   var checkAuth = function () {
+    // parses out boolean isAuth from cookie
     $scope.isAuth = !!Auth.isAuth( "isAuth" );
+    // parses out usernmae from cookie
     $scope.username = Auth.isAuth( "username" );
   };
 
